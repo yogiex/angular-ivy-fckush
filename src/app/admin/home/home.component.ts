424e522fd5c13ec09c1db26b1f680af3ac3f5fec
yogiex <priyagung.yogi@yahoo.com>
2022-04-01 19:09:27 +0700

    5 files modified

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -10,15 +10,18 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.myChart = new Chart('myChart', this.data);
+    this.addData();
+    this.removeData();
+    console.log(this.data);
   }
   myChart;
   data: any = {
-    type: 'bar',
+    type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Januari', 'Februari', 'Maret', 'April', 'Juni', 'Juli'],
       datasets: [
         {
-          label: '# of Votes',
+          label: 'Requests',
           data: [12, 19, 3, 5, 2, 3],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
@@ -48,4 +51,31 @@ export class HomeComponent implements OnInit {
       },
     },
   };
+
+  addData() {
+    window.setInterval(() => {
+      const rndInt = Math.floor(Math.random() * 100) + 1;
+      const randomTime = Date.now().toString(10).substring(7);
+      this.data.data.datasets.forEach((val) => {
+        val.data.push(rndInt);
+        console.log(rndInt);
+        
+      });
+      this.data.data.labels.push(randomTime)
+      this.myChart.update();
+    }, 3000);
+
+    
+  }
+  updateData() {}
+
+  removeData() {
+    window.setInterval(() => {
+      this.data.data.labels.shift();
+      this.data.data.datasets.forEach((datasets) => {
+        datasets.data.shift();
+      });
+      this.myChart.update();
+    }, 5000);
+  }
 }
